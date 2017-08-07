10e1ce9f059277544c48fd4955923a270e67fd51
miguel-acevedo <acevedomiguel@outlook.com>
2017-08-07 12:18:05 -0700

    Added minify for JS files

--- minify.py
+++ minify.py
@@ -1,19 +1,27 @@
 import os
 import requests
-url = 'https://cssminifier.com/raw' #Calls the minifier url
+css = 'https://cssminifier.com/raw' #Calls the minifier url
+js = 'https://javascript-minifier.com/raw'
+
+def minifyCode(url, path):
+    print(path)
+    data = {'input': open(path, 'rb').read()} # Packs the css file to a data variable to be sent off to the minifier.
+    response = requests.post(url, data=data) # Sends a Post requests, then retrieves the data.
+
+    wr = open(path, 'w') # Opens current local file.
+    wr.write(response.text) # Overwrites the local file.
 
 def find_files(directory): #Function to recursivly loop through all files and minify.
     for filename in os.listdir(directory):
-        if (os.path.isdir(os.path.join(directory, filename))): #Checks if the file is directory
-            find_files(os.path.join(directory, filename))
-        if filename.endswith(".css"): # Checks for css files
-            path = os.path.join(directory, filename) # Creates path
-            print(path)
-            data = {'input': open(path, 'rb').read()} # Packs the css file to a data variable to be sent off to the minifier.
-            response = requests.post(url, data=data) # Sends a Post requests, then retrieves the data.
+        path = os.path.join(directory, filename) # Creates path
 
-            wr = open(path, 'w') # Opens current local file.
-            wr.write(response.text) # Overwrites the local file.
+        if (os.path.isdir(path)): #Checks if the file is directory
+            find_files(path)
+        if filename.endswith(".css"): # Checks for css files
+            minifyCode(css, path)
+            continue
+        elif filename.endswith(".js"): # Checks for css files
+            minifyCode(js, path)
             continue
         else:
             continue
